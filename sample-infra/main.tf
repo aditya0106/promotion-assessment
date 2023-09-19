22d478d5e99aa33747f32778ecc18a09c4dc3f02
aditya_gupta03 <aditya.gupta@tungsten-network.com>
2023-09-19 23:45:22 +0530

    sample infra

---
+++ sample-infra/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-backend-state-aditya"
+    key    = "s3-bucket/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+}
+provider "aws" {
+  region = "ap-south-1"
+  default_tags {
+    tags = {
+      "Project" = "Promotion Assessment"
+      "Environment"    = "Dev"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+}
